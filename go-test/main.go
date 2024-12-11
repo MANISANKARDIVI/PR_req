7be03fff592d5e75bc0a88efaf2ea1af0f332ec3
DIVI MANI SANKAR <manisankar.divi@gmail.com>
2024-12-11 12:23:59 +0530

    Update main.go

--- go-test/main.go
+++ go-test/main.go
@@ -42,7 +42,7 @@ func errorHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.WithFields(log.Fields{
 		"animal": "walrus",
-		"size":   100,
+		"size":   10,
 	}).Info("A group of walrus emerges from the ocean")
 
 	// Setting up handlers
